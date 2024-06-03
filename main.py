49e1a95a84c3a3ec3d94c693898056392caa8596
Marxu <fuhaojun@52tt.com>
2024-06-03 15:55:17 +0800

    feat: 使用 for 循环 按照有几页 生成空白页

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
     pdf.line(10, 21, 200, 21)
+
+    # 按照有几页 生成空白页
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
 pdf.output('output.pdf')
