ff4ce3dbeb98b737387c8a3d48b777e76b917438
Marxu <fuhaojun@52tt.com>
2024-06-03 17:52:05 +0800

    feat: 优化一下 去掉初始的双条线

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', style='I', size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
 
     al.add_lines(pdf)
 
